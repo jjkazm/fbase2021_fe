c3016cea0f38e70690155f0809ca715bd8cca3b8
Kuba Kazmierczak <jakub.kazmierczak@netguru.com>
2021-02-08 22:19:16 +0100

    Add firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase";
 
-function App() {
+const App = () => {
+  const firebaseApp = firebase.apps[0];
+
+
+const authenticate = () => {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth()
+    .signInWithRedirect(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+  
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+
+      console.log(`token: ${token}
+      credential: ${credential}
+      user: ${user}`)
+      // ...
+    }).catch((error) => {
+
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+      console.log('ERROR')
+    });
+}
   return (
     <div className="App">
       <header className="Firebase test">
@@ -10,6 +43,14 @@ function App() {
           Just a simple app for checking out Firebase
         </h1>
       </header>
+      <button
+      onClick={ authenticate }
+      >
+        Login
+      </button>
+      <code>
+        <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
+      </code>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBoACh5ugZSnZbTFU7QnoZlKQViaDaDivs",
+  authDomain: "fbase-2021-test.firebaseapp.com",
+  databaseURL: "https://fbase-2021-test-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fbase-2021-test",
+  storageBucket: "fbase-2021-test.appspot.com",
+  messagingSenderId: "45690471389",
+  appId: "1:45690471389:web:4d6571b639acf7ff2aac34"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
